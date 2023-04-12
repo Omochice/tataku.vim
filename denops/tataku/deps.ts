9ab9679da15df90486c198c614e2318ae17929fd
denopendabot <denopendabot@github.com>
2023-04-12 04:19:48 +0000

    build(deps): bump denops_std from v3.8.1 to v4.1.4

--- denops/tataku/deps.ts
+++ denops/tataku/deps.ts
@@ -1,6 +1,6 @@
-export type { Denops } from "https://deno.land/x/denops_std@v3.8.1/mod.ts";
-export * as op from "https://deno.land/x/denops_std@v3.8.1/option/mod.ts";
-export * as fn from "https://deno.land/x/denops_std@v3.8.1/function/mod.ts";
+export type { Denops } from "https://deno.land/x/denops_std@v4.1.4/mod.ts";
+export * as op from "https://deno.land/x/denops_std@v4.1.4/option/mod.ts";
+export * as fn from "https://deno.land/x/denops_std@v4.1.4/function/mod.ts";
 export {
   ensureArray,
   ensureObject,
