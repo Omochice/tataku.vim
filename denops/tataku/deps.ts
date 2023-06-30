05b3d751952b69b44e0dcadbd0812bae731a284f
denopendabot <denopendabot@github.com>
2023-06-30 00:37:58 +0000

    build(deps): bump denops_std from v5.0.0 to v5.0.1

--- denops/tataku/deps.ts
+++ denops/tataku/deps.ts
@@ -1,6 +1,6 @@
-export type { Denops } from "https://deno.land/x/denops_std@v5.0.0/mod.ts";
-export * as op from "https://deno.land/x/denops_std@v5.0.0/option/mod.ts";
-export * as fn from "https://deno.land/x/denops_std@v5.0.0/function/mod.ts";
+export type { Denops } from "https://deno.land/x/denops_std@v5.0.1/mod.ts";
+export * as op from "https://deno.land/x/denops_std@v5.0.1/option/mod.ts";
+export * as fn from "https://deno.land/x/denops_std@v5.0.1/function/mod.ts";
 export {
   ensureArray,
   isArray,
