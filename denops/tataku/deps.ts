c23cd0cd3e4dbcc914d99505dc1119b52772231e
Omochice <mochice.mls.ntl@gmail.com>
2024-07-07 17:07:57 +0900

    chore: use @core/unknownutil instead

--- denops/tataku/deps.ts
+++ denops/tataku/deps.ts
@@ -1,10 +1,7 @@
 export type { Denops } from "https://deno.land/x/denops_std@v6.5.0/mod.ts";
 export * as op from "https://deno.land/x/denops_std@v6.5.0/option/mod.ts";
 export * as fn from "https://deno.land/x/denops_std@v6.5.0/function/mod.ts";
-export {
-  is,
-  type PredicateType,
-} from "https://deno.land/x/unknownutil@v3.18.1/mod.ts";
+export { is, type PredicateType } from "jsr:@core/unknownutil@3.18.1";
 export { join } from "jsr:@std/path@0.225.1";
 
 export { toFileUrl } from "jsr:@std/path@0.225.1";
