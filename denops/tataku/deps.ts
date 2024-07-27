0ba6fac31fc09ed3f9b2b37bf54ab24cf0ab858f
Omochice <44566328+Omochice@users.noreply.github.com>
2024-07-27 15:49:15 +0900

    chore: migrate jsr:@denops/std v7 (#164)

--- denops/tataku/deps.ts
+++ denops/tataku/deps.ts
@@ -1,6 +1,6 @@
-export type { Denops } from "https://deno.land/x/denops_std@v6.5.1/mod.ts";
-export * as op from "https://deno.land/x/denops_std@v6.5.1/option/mod.ts";
-export * as fn from "https://deno.land/x/denops_std@v6.5.1/function/mod.ts";
+export type { Denops } from "jsr:@denops/std@7.0.0";
+export * as op from "jsr:@denops/std@7.0.0/option";
+export * as fn from "jsr:@denops/std@7.0.0/function";
 export { is, type PredicateType } from "jsr:@core/unknownutil@3.18.1";
 export { join } from "jsr:@std/path@1.0.2";
 
