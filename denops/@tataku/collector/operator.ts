e8cbb52c55f1621277da4651f123ed68df5884f1
Omochice <mochice.mls.ntl@gmail.com>
2024-07-30 00:07:49 +0900

    refactor: use collector factory type

--- denops/@tataku/collector/operator.ts
+++ denops/@tataku/collector/operator.ts
@@ -1,11 +1,12 @@
 import type { Denops } from "jsr:@denops/std@7.0.0";
 import { is } from "jsr:@core/unknownutil@3.18.1";
+import type { CollectorFactory } from "../../tataku/export.ts";
 
 const isOption = is.ObjectOf({
   selected: is.ArrayOf(is.String),
 });
 
-const collector = (_: Denops, options: unknown) => {
+const collector: CollectorFactory = (_: Denops, options: unknown) => {
   if (!isOption(options)) {
     throw new Error(
       `:Internal error: options is invalid ${JSON.stringify(options)}`,
